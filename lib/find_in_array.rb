174d4eeeaf31b3fabdb7bb179f1ac422a3a14944
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-25 14:51:26 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  if array.detect(value_to_find) == true
+  if array.include?(value_to_find) == true
     return array.index(value_to_find)
   else return nil
   end# Add your solution here
